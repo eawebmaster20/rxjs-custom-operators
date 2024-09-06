c1d4859f3c6b9e5d9897743b692713fc923c85ac
IT Compliance <compliance@amalitech.org>
2024-09-06 10:40:40 +0000

    feat: add template to

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { of } from 'rxjs';
+import { from, of } from 'rxjs';
 import { simulateNetworkLatency } from './utilities/rxjs-custom-operators/simulateNetworkLatency';
 import { countEmissions } from './utilities/rxjs-custom-operators/trackObservableEmmisions';
 import { filterByIndex } from './utilities/rxjs-custom-operators/filterNthEmmitions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'rxjs-custorm-operators';
-  source$ = of(1, 2, 3, {info:'be rest assured, the operators are custom'}, 'Bra larsh');
+  originalData=[1, 2, 3, {info:'be rest assured, the operators are custom'}]
+  transformedData: any[] = [];
+  sourceObs = of(1, 2, 3, {info:'be rest assured, the operators are custom'}, 'Bra lash');
   constructor(){
-    this.source$.pipe(
+    this.sourceObs.pipe(
       simulateNetworkLatency(1000),
       countEmissions(),
       filterByIndex(index => index % 2 === 0)
     )
     .subscribe({
       next: (value) =>{ 
-        console.log(value[0])
-        console.log( 'Emmited: '+ value[1] + 'values so far')
+        console.log(value[0]);
+        this.transformedData.push(value);
       },
       error: (error) => console.error('Error:', error),
       complete: () => console.log('Completed')
